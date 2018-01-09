30054a9d2cf165e72b3c55d4436f5f28b7018129
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 23:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var recipes = {
     delete recipes.flour;
     return recipes
   }
-  function destructivelyDeleteFromObjectByKey(recipes, flour) {
-    delete recipes.flour;
-    return recipes 
+  function destructivelyDeleteFromObjectByKey(object, key) {
+    delete object.key;
+    return object  
   }
